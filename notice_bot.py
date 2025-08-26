1cca33d59c7ff21f0c82bfc4163031aa10008324
eorhdvh <happy22@snu.ac.kr>
2025-08-26 22:12:07 +0900

    Update notice_bot.py

--- notice_bot.py
+++ notice_bot.py
@@ -7,7 +7,7 @@ import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 DISCORD_WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL")
-URL = "https://me.snu.ac.kr/%ea%b3%b5%ed%86%b5-%ea%b3%b5%ec%a7%80%ec%82%ac%ed%95%ad/"
+URL = "https://me.snu.ac.kr/%ed%95%99%eb%b6%80-%ea%b3%b5%ec%a7%80%ec%82%ac%ed%95%ad/"
 
 def fetch_notices():
     headers = {
